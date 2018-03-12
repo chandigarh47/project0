b5edd09802faf0b4250889128977c33a597ccfb2
gurdeep kaur <gurdeepkaur@gurdeeps-MacBook-Pro.local>
2018-03-12 14:19:59 +1100

    project0

---
+++ js/main.js
@@ -0,0 +1,34 @@
+console.log('tic tac toe');
+
+
+const player1="X";
+const player2="O";
+const token="player1";
+
+const game=[
+  [''],[''],[''],
+  [''],[''],[''],
+  [''],[''],['']
+
+];
+
+const gameBoard = function(cell){
+  game[cell]="X";
+
+};
+
+const twoPlayers = function(cell){
+
+const player1 = (player1 === "X")? "O":"X";
+
+}
+
+
+$(document).ready(function(){
+
+
+  $('.play').click(function(){
+    $('#announcement').html('Game Started').fadeOut(3000);
+
+  });
+});
