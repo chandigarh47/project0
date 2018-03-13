301981f9c554ea099ecb2a141fe6d926f4af5567
gurdeep kaur <gurdeepkaur@192-168-1-137.tpgi.com.au>
2018-03-13 22:09:13 +1100

    modified work

---
+++ README.md
@@ -0,0 +1,53 @@
+# Project 0
+
+## subheadings
+
+some **regular** text.
+
+
+
+some _regular_ text
+
+this is a new paragraph
+
+## lists
+<ul>
+<li></li>
+</ul>
+
+* list Item
+* second item
+    * sub items
+
+    ### marx brothers
+    1.groucho
+    2.harpo
+    3.chico
+
+    ![bill murray](https://)
+
+    https://twitter.com
+
+    [twitter or whatever](https://twitter.com)
+
+    to quote some famous:
+    >famous inspirational quote
+
+    don't find it is inspirational
+
+    you should use `<section>` tag.
+
+    there are two many variables called `score` in this project.
+
+
+    ## Github flavoured markdown
+    ### function expression example
+
+    ```javascript
+
+    const sayHello = function(){
+      console.log('hello');
+    }
+    ```
+    ~~ this is struck out ~~ updated docs
+    
--- js/main.js
+++ js/main.js
@@ -1,34 +1,82 @@
 console.log('tic tac toe');
 
 
+
 const player1="X";
 const player2="O";
-const token="player1";
-
-const game=[
-  [''],[''],[''],
-  [''],[''],[''],
-  [''],[''],['']
-
+let player1Next = true;
+const gameBoard=[
+  '','','',
+  '','','',
+  '','',''
 ];
 
-const gameBoard = function(cell){
-  game[cell]="X";
 
-};
+const playTurn = function(cell){
+  if (player1Next) {
+    gameBoard[cell] = "X";
+    player1Next = false;
+    } else {
+      gameBoard[cell] = "O";
+      player1Next = true;
+    }
 
-const twoPlayers = function(cell){
+      console.log(gameBoard);
 
-const player1 = (player1 === "X")? "O":"X";
 
-}
+      if(hasGameWin() === true){
+
+        console.log('Hurray!! You Won The Game');
+      } else {
+
+        console.log('Try Again');
+      };
+      return gameBoard[cell];
+
+    };
+
+
+  const hasGameWin = function(){
+
+    if(
+      (gameBoard[0] === player1 && gameBoard[1] === player1 && gameBoard[2] === player1)||
+      (gameBoard[3] === player1 && gameBoard[4] === player1 && gameBoard[5] === player1)||
+      (gameBoard[6] === player1 && gameBoard[7] === player1 && gameBoard[8] === player1)||
+      (gameBoard[0] === player1 && gameBoard[3] === player1 && gameBoard[6] === player1)||
+      (gameBoard[1] === player1 && gameBoard[4] === player1 && gameBoard[7] === player1)||
+      (gameBoard[2] === player1 && gameBoard[5] === player1 && gameBoard[8] === player1)||
+      (gameBoard[0] === player1 && gameBoard[4] === player1 && gameBoard[8] === player1)||
+      (gameBoard[2] === player1 && gameBoard[4] === player1 && gameBoard[6] === player1)
+
+    )
+        {
+
+      return true;
+        }  else {
+      return false;
+        }
+
+        }
 
 
 $(document).ready(function(){
 
+    $('.play').click(function(){
+      $('#announcement').html('Game Started').fadeOut(3000);
 
-  $('.play').click(function(){
-    $('#announcement').html('Game Started').fadeOut(3000);
+});
 
-  });
+    $('.box').click(function(event){
+      var $element = $(this);
+      var keyword1 = playTurn(this.id);
+      $element.html(keyword1);
+
+
+    // for(let i = 0; i < gameBoard.length; i++){
+    // document.getElementsByTagName().innerHTML('gameboard[i]');
+    // }
+    });
+
+
+
 });
