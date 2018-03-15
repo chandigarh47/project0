b834cc053e45940a370ccf330ed25456246d64aa
gurdeep kaur <gurdeepkaur@gurdeeps-MacBook-Pro.local>
2018-03-16 10:36:49 +1100

    tic tac toe game

--- README.md
+++ README.md
@@ -1,5 +1,81 @@
 # Project 0
 
+This is a tic-tac-toe game developed using HTML, CSS, Javascript and jQuery.
+
+## Getting Started
+
+Player1 starts with the player1: X. Player2 starts with player2. Each click will switch the player. Once there is a winner or a draw the game can be restarted by clicking on the Reset button.
+
+### Game Features:
+
+* Each click changes the player.
+* Game result is displayed using css animate.
+* Tic-tac-toe gameboard is replicated in an array for checking the game status.
+
+### Prerequisites
+
+None.
+
+### Installing
+
+The game runs on the browser. No installation required.
+
+## Built With
+
+* [jQuery](http://jquery.com)
+
+
+## Versioning
+
+For the versions available, see the [tags on this repository](https://github.com/chandigarh47/project0).
+
+## Authors
+
+* **Gurdeep Kaur** - *Initial work* - [Gurdeep's Repository](https://github.com/chandigarh47/project0)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+jquery.com
+jQuery
+jQuery: The Write Less, Do More, JavaScript Library
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- # Project 0
+
 ## subheadings
 
 some **regular** text.
@@ -50,4 +126,4 @@ this is a new paragraph
     }
     ```
     ~~ this is struck out ~~ updated docs
-    
+     -->
--- js/main.js
+++ js/main.js
@@ -1,42 +1,54 @@
 console.log('tic tac toe');
 
 
-
+//Declare two constant variable and assigning "X" and "O" values to them.
 const player1="X";
 const player2="O";
 let player1Next = true;
-const gameBoard=[
+
+// declare a null array and named as gameBoard
+const gameBoard = [
   '','','',
   '','','',
   '','',''
 ];
 
+// switiching the values between player1 and player2 in a function called playTurn
 
-const playTurn = function(cell){
-  if (player1Next) {
-    gameBoard[cell] = "X";
-    player1Next = false;
+const playTurn = function( cell ) {
+
+// In if condition we are checking if player1 is true then prints "X" and immediately becomes
+// return false to give chance to player2 to play its turn.
+  if ( player1Next ) {
+      gameBoard[ cell ] = "X";
+      player1Next = false;
     } else {
-      gameBoard[cell] = "O";
+      gameBoard[ cell ] = "O";
       player1Next = true;
     }
 
-      console.log(gameBoard);
+      console.log( gameBoard );
+// In this if condition checking the hasGameWin function if it is true for player1 THEN
+// prints console otherwise run else part.
 
+      if( hasGameWin() === true ) {
 
-      if(hasGameWin() === true){
+        //console.log('Hurray!! You Won The Game');
+        $('#announcement').html('Hurray!! You Won The Game').fadeIn(1) ;
 
-        console.log('Hurray!! You Won The Game');
       } else {
-
-        console.log('Try Again');
+        console.log( 'Try Again' );
       };
-      return gameBoard[cell];
 
-    };
+        return gameBoard[ cell ];
+
+      };
 
 
-  const hasGameWin = function(){
+
+// we declare a hasGameWin function for checking the matches in 3 rows
+// 3 columns and 2 diagonals if match found then return true for player1
+  const hasGameWin = function() {
 
     if(
       (gameBoard[0] === player1 && gameBoard[1] === player1 && gameBoard[2] === player1)||
@@ -47,36 +59,80 @@ const playTurn = function(cell){
       (gameBoard[2] === player1 && gameBoard[5] === player1 && gameBoard[8] === player1)||
       (gameBoard[0] === player1 && gameBoard[4] === player1 && gameBoard[8] === player1)||
       (gameBoard[2] === player1 && gameBoard[4] === player1 && gameBoard[6] === player1)
-
     )
         {
+          return true;
+    }
+  //in else part if player 2 got a match then return true
+     else if (
+      (gameBoard[0] === player2 && gameBoard[1] === player2 && gameBoard[2] === player2)||
+      (gameBoard[3] === player2 && gameBoard[4] === player2 && gameBoard[5] === player2)||
+      (gameBoard[6] === player2 && gameBoard[7] === player2 && gameBoard[8] === player2)||
+      (gameBoard[0] === player2 && gameBoard[3] === player2 && gameBoard[6] === player2)||
+      (gameBoard[1] === player2 && gameBoard[4] === player2 && gameBoard[7] === player2)||
+      (gameBoard[2] === player2 && gameBoard[5] === player2 && gameBoard[8] === player2)||
+      (gameBoard[0] === player2 && gameBoard[4] === player2 && gameBoard[8] === player2)||
+      (gameBoard[2] === player2 && gameBoard[4] === player2 && gameBoard[6] === player2)
+    )
+        {
+          return true;
+    }
 
-      return true;
-        }  else {
-      return false;
+// here i am checking the values of gameBoard using counter assign to 0.
+// checking each element of gameBoard through loop.
+    let counter = 0;
+    for ( let i = 0; i < gameBoard.length; i++ ) {
+
+// here if gameBoard is not equal to null then increment the values of counter by 1.
+        if( gameBoard[ i ] != "" )
+        {
+          counter += 1;
+
         }
+    }
 
-        }
+// here if counter is equal to 9 cells of gameboard and there is no match found by player1 and player2
+//then it returns draw.
+    if( counter === 9 ) {
+      //console.log(' draw' );
+       $( '#announcement' ).html( 'draw' ).fadeIn( 1 );
+      return "draw";
+    }
 
+}
 
-$(document).ready(function(){
+// jquery starts here
+  $(document).ready(function() {
 
-    $('.play').click(function(){
-      $('#announcement').html('Game Started').fadeOut(3000);
+//firstly select the id name box where i want to perform the event by clicking.
+    $( '.box' ).click(function( event ) {
 
-});
+// ${this} it refers to the gameBoard[cell] and storing into a variable.
+      var $element = $( this );
 
-    $('.box').click(function(event){
-      var $element = $(this);
-      var keyword1 = playTurn(this.id);
-      $element.html(keyword1);
+//   here calling the playTurn function with gameBoard[cell]id  in its parantheses.
+      var keyword1 = playTurn( this.id );
+      $element.html( keyword1 );
 
-
-    // for(let i = 0; i < gameBoard.length; i++){
-    // document.getElementsByTagName().innerHTML('gameboard[i]');
-    // }
     });
 
+// this click is for button when i click on button name play a game
+// the game started appears on the screen
 
+    $('.play').click(function() {
+      $('#announcement').html('Game Started').fadeOut(3000);
 
-});
+    });
+
+// here i am doing the reset the values of gameBoard when i click the button reset.
+
+    $('.reset').click(function() {
+      for ( let i = 0; i < gameBoard.length; i++ ) {
+        gameBoard[ i ] = "";
+        $('#' + i).html("")
+      }
+      $('#announcement').html('Game Started').fadeOut(3000);
+
+    });
+
+    });
